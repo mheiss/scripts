240b2a2b73b6564f8ee72db3b753bb778247f5e4
Michael Heiß <michael.heiss@outlook.at>
2026-01-18 11:17:46 +0100

    Initial version
    Script that walks through all hosts and invokes apt-get update and a custom update.ps1 script if present.

---
+++ update.ps1
@@ -0,0 +1,66 @@
+# Function that runs the given process and prints the result to the console
+function RunCommands {
+    param(
+        [string]$user,
+        [string]$destination,
+        [string]$command
+    )
+    
+    $arguments = @("$user@$destination", $command)
+    $argumentsString = $arguments -join " "
+
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = "ssh"
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $argumentsString
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $stdout = $p.StandardOutput.ReadToEnd()
+    $p.WaitForExit()
+    Write-Host "$stdout"
+}
+
+# Writes out a nice header 
+function Write-Header {
+    param(
+        [string]$header
+    )
+    for ($i = 0; $i -le $header.Length; $i++) {
+        if ($i -le $header.Length - 1) {
+            Write-Host -NoNewline "-"
+        }
+        else {
+            Write-Host "-"
+        }
+    }
+    Write-Host $header
+    for ($i = 0; $i -le $header.Length; $i++) {
+        if ($i -le $header.Length - 1) {
+            Write-Host -NoNewline "-"
+        }
+        else {
+            Write-Host "-"
+        }
+    }
+}
+
+##-----------------
+# Update all VMs
+##------------------
+$vms = 'evcc', 'influx', 'dyndns', 'traefik', 'grafana', 'keycloak', "oauth2-proxy", "zwave", "openhab5", "dashboard"
+foreach ($vm in $vms) {
+    $user = "root"
+    $destination = "$vm.heiss.lan"
+    $aptUpdate = "DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade --yes"
+    $appUpdate = "[ -f update.ps1 ] && pwsh ./update.ps1"
+
+    Write-Header "Updating $destination"
+
+    RunCommands -user $user -destination $destination -command $aptUpdate
+    Write-Host ""
+    RunCommands -user $user -destination $destination -command $appUpdate
+}
+
