61de7beba43ad3d2115155e85771fbe9e23a5436
Michael Heiss <michael.heiss@ssi-schaefer.com>
2026-06-18 22:53:06 +0200

    Zwave: Update symlink path

--- scripts/zwave/update.ps1
+++ scripts/zwave/update.ps1
@@ -2,6 +2,6 @@ $Repo = "zwave-js/zwave-js-ui"
 $AssetName = "linux\.zip$"
 $BaseDir = "/opt/zwave-js"
 $ServiceName = "zwave-js"
-$SymlinkPath = "/sbin/zwave-js-ui-linux"
+$SymlinkPath = "/sbin/zwave-js"
 
 Update-App -Repo $Repo -AssetName $AssetName -BaseDir $BaseDir -ServiceName $ServiceName -SymlinkPath $SymlinkPath
